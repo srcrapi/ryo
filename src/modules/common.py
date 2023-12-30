5046d963eaad92e4949cd95ceb2d5d9ca21546b6
srcrapi <rafa1000.skylanders@gmail.com>
2023-12-30 06:41:41 +0000

    feat: regular colors and bold colors

--- src/modules/common.py
+++ src/modules/common.py
@@ -14,6 +14,24 @@ ideSelected = False
 
 # Colors
 
+#Regular colors
+white='\033[0m'
+red='\033[0;31m'
+green='\e[0;32m'
+cyan='\033[0;96m'
+orange='\e[0;33m'
+blue='\e[0;34m'
+purple='\e[0;35m'
+
+#Bold colors
+
+bred='\033[1;31m'
+bgreen='\e[1;32m'
+bcyan='\033[1;96m'
+borange='\e[1;33m'
+bblue='\e[1;34m'
+bpurple='\e[1;35m'
+
 # Functions
 
 def showLogo():
